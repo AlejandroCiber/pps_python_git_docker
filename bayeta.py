e0affd815bcf972d9ff960b3ff6506b7ce1e1a21
Alejandro Navarro <alexrmdrid@gmail.com>
2026-02-04 16:52:06 +0100

    Frotar devuelve frases aleatorias

--- bayeta.py
+++ bayeta.py
@@ -1,4 +1,13 @@
-# bayeta.py
+import random
+
+frases = [
+    "Hoy será un gran día",
+    "La fortuna te sonríe",
+    "Sorpresas agradables están por venir",
+    "Confía en tu intuición",
+    "Una nueva oportunidad se acerca"
+]
 
 def frotar(n_frases: int = 1) -> list:
-    pass
+    return random.choices(frases, k=n_frases)
+
