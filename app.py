35d7f52d2610789af0c686f32a9e05e4fd19fc2b
Alejandro Navarro <alexrmdrid@gmail.com>
2026-02-04 16:42:40 +0100

    Añadido bayeta.py, Flask app y endpoint /frotar

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hola, mundo")
+from flask import Flask, jsonify
+from bayeta import frotar
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hola, mundo"
+
+@app.route('/frotar/<int:n_frases>')
+def frotar_endpoint(n_frases):
+    # Por ahora devuelve la misma frase repetida N veces
+    return jsonify(frotar(n_frases))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ bayeta.py
@@ -0,0 +1,3 @@
+def frotar(n_frases: int = 1) -> list:
+    frase = "Hoy será un gran día"
+    return [frase] * n_frases
